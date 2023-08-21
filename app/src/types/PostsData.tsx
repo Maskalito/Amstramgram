016017215be1cc3bd389e94a8e25c40a8613ffef
ClementBarrier <clement.barrier@epitech.eu>
2023-08-21 16:19:02 +0200

    [ADD] interfaces for data

---
+++ app/src/types/PostsData.tsx
@@ -0,0 +1,13 @@
+interface PostData {
+  id: number;
+  username: string;
+  user_image: string;
+  post_image: string;
+  description: string;
+  posted_time: string;
+  likes: number;
+  shares: number;
+  user_id: number;
+}
+
+export default PostData;
---
+++ app/src/types/UserData.tsx
@@ -0,0 +1,9 @@
+interface UserData {
+  id: number;
+  username: string;
+  email: string;
+  password: string;
+  user_url: string;
+}
+
+export default UserData;
